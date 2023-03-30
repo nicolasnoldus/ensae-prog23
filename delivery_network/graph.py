3be25f1b99016e49e417342f8d5a3b2cc92eb5dd
nicolasnoldus <126243712+nicolasnoldus@users.noreply.github.com>
2023-03-30 09:14:49 +0200

    Update graph.py

--- delivery_network/graph.py
+++ delivery_network/graph.py
@@ -75,26 +75,47 @@ class Graph:
 
 
     def get_path_with_power(self, src, dest, power):
+
         ancetres = self.bfs(src, dest, power) #ancetres est encore le dicitonnaire qui a comme clé un noeud et comme valeur 
+
         #celui par lequel on a pu parvenir à ce noeud.
+
         parcours = []
+
         #on crée une liste parcours qui nous donnera le parcours entre les deux noeuds choisis en respectant toujours la puissance 
+
         #exigée.
+
         a = dest #on renomme dest en a pour faciliter la suite.
+
         if a not in ancetres:
+
             return None
+
             #s'il n'y a pas de graphe connexe avec la puissance exigée, on retourne none.
+
         while a != src:
+
             #On met cette condition car on part de l'arrivée de notre bfs, donc on remonte point par point jusqu'à arriver à 
+
             #notre point de départ.
+
             parcours.append(a)
+
             a = ancetres[a]
+
             #on rajoute le sommet a à notre liste de noeuds parcourus et on définit "le nouveau" a comme étant son ancêtre pour
+
             #rebrousser chemin.
+
         parcours.append(src)
+
         #on doit ajouter l'origine a la main parce que notre boucle while s'arrête dès lors que a prend la valeur du noeud de départ
+
         #et ne le rajoute donc pas dans la liste.
+
         parcours.reverse()
+
         return parcours
 
 
@@ -181,38 +202,71 @@ class Graph:
                 power = i[3]
         return power
     
-    def min_power(self, src, dest):
+     def min_power(self, src, dest):
+
         debut = 1
+
         fin = self.max_power
-        if dest not in self.dfs(src, [], []):
+
+        actu=self.get_path_with_power(src, dest, self.max_power)
+
+        if actu is None or dest not in actu:
+
             return None, None
+
         #si les deux noeuds en question ne sont pas sur un graphe connexe, on retourne none car il n'y a pas de chemins possible. 
+
         while debut != fin: 
+
             #on fait une recherche binaire. 
+
             #Pour être tout à fait honnête, la condition sur le while est un peu désuète étant donné qu'on fait un break
+
             #avant que cette condition puisse se remplir mais c'est la solution qui a le mieux marché sur plusieurs tests : 
+
             #network.1 et network.2, lentement mais sûrement.
+
             mid = ((debut+fin)//2)
-            actu=self.dfs(src, [], [], power=mid)
+
+            actu=self.get_path_with_power(src, dest, power=mid)
+
             #on actualise à chaque itération le graphe des sommets formant un graphe connexe et permettant un chemin. 
-            if dest in actu:
+
+            if actu is not None and dest in actu:
+
                 fin = mid
+
             #si le sommet qu'on veut atteindre est dans le graphe fait à partir de la médiane des puissances
+
             #on redéfinit la "borne sup" comme étant l'ancien milieu pour retrécir notre champ de recherche.
-            elif dest not in actu:
+
+            else:
+
                 debut=mid
+
             #on procède pareillement mais avec la plus petite puissance dans le cas contraire.
+
             if fin-debut == 1 :
+
                 break
+
             #Comme on ne prend pas comme valeurs de power les puissances présentes dans le graphe mais simplement 
+
             #les entiers situés entre la plus grande puissance et la plus petite, 
+
             #la condition pour sortir de la boucle while est que la différence entre les deux extrêmes soit égale à un. 
+
             #Ainsi, cela signifierait que ce sont deux entiers qui se suivent et on doit donc nécessairement prendre 
+
             #"fin" car début serait trop petit. 
+
         minus=fin
+
         return self.get_path_with_power(src, dest, minus), minus
+
         #en testant cette fonction sur le network.2 avec comme noeuds 1 et 12, voici le résultat obtenu :
-        # ([1, 2, 4, 12], 52761). C'est assez long mais il parvient au résultat sans trop de soucis. 
+
+        # ([1, 2, 4, 12], 52761). C'est assez long mais il parvient au résultat sans trop de soucis.  
 
     def connected_components_set(self):
         return set(map(frozenset, self.connected_components()))
